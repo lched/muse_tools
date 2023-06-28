Fix data streaming, add option to send features averaged over channels or not

muse_to_osc.py
@@ -23,8 +23,10 @@ FFT_COMPUTE_RATE = 10
 FREQUENCY_BANDS_RATE = 10
 CHECK_STREAM_RATE = 10
 
-# FFT parameters
+# FEATURES PARAMETERS
 N_FFT = 256
+# Should the spectral features be sent as mean values over channels?
+SEND_MEAN_FEATURES = True
 
 
 def single_lsl_stream_to_osc(stream_type):
@@ -44,17 +46,18 @@ def single_lsl_stream_to_osc(stream_type):
             osc_client.send_message(f"/muse/{stream_type.lower()}", samples[0])
 
 
-def eeg_stream_to_osc(stream_type, remove_aux):
+def eeg_stream_to_osc(stream_type, use_aux):
     stream = pylsl.resolve_byprop("type", stream_type, timeout=LSL_SCAN_TIMEOUT)[0]
     # Create LSL inlet
     inlet = pylsl.StreamInlet(stream, max_chunklen=LSL_MAX_SAMPLES)
     n_channels = inlet.info().channel_count()  # Remove last channel
-    if remove_aux:
+    if not use_aux:
         n_channels -= 1
     sample_rate = inlet.info().nominal_srate()
     stream_running = True
 
-    data = np.zeros((N_FFT, n_channels))
+    fft_buffer = np.ones((N_FFT, n_channels)) * 1e-6
+    incoming_data = np.zeros(n_channels)
 
     # Create an OSC client
     osc_client = udp_client.SimpleUDPClient(OSC_IP, OSC_PORT)
@@ -66,30 +69,43 @@ def eeg_stream_to_osc(stream_type, remove_aux):
             t = time.time() + 1 / FFT_COMPUTE_RATE
             if stream_running:
                 with shared_list_lock:
-                    fft = np.abs(np.fft.rfft(data, axis=0))
-                osc_client.send_message("/muse/eeg_fft", list(np.mean(fft, axis=-1)))
+                    fft = np.abs(np.fft.rfft(fft_buffer, axis=0))
+                if SEND_MEAN_FEATURES:
+                    osc_client.send_message(
+                        "/muse/eeg_fft", list(np.mean(fft, axis=-1))
+                    )
+                else:
+                    osc_client.send_message("/muse/eeg_fft", list(fft))
             time.sleep(max(0, t - time.time()))
 
     # Create threads that will compute FFT and send results via OSC
     def get_power_band_to_osc_fn(power_band_name, freq_min, freq_max):
         def power_band_to_osc():
+            idx_min = int(freq_min * N_FFT / sample_rate)
+            idx_max = int(freq_max * N_FFT / sample_rate)
             while True:
                 t = time.time() + 1 / FREQUENCY_BANDS_RATE
-                if stream_running:
-                    with shared_list_lock:
-                        data = np.mean(
-                            fft[
-                                int(freq_min * N_FFT / sample_rate) : int(
-                                    freq_max * N_FFT / sample_rate
-                                )
-                            ]
-                        )
-                    osc_client.send_message(
-                        f"/muse/features/{power_band_name}_absolute", data
-                    )
-                    osc_client.send_message(
-                        f"/muse/features/{power_band_name}_relative", data / np.sum(fft)
-                    )
+
+                if not stream_running:
+                    continue
+
+                # Get FFT relevant FFT bins from all channels
+                with shared_list_lock:
+                    power = fft[idx_min:idx_max]
+                absolute_power = np.sum(power, axis=0)
+                relative_power = absolute_power / np.sum(fft, axis=0)
+
+                if SEND_MEAN_FEATURES:
+                    # Compute mean over channels
+                    absolute_power = np.mean(absolute_power)
+                    relative_power = np.mean(relative_power)
+
+                osc_client.send_message(
+                    f"/muse/features/{power_band_name}_absolute", absolute_power
+                )
+                osc_client.send_message(
+                    f"/muse/features/{power_band_name}_relative", relative_power
+                )
                 time.sleep(max(0, t - time.time()))
 
         return power_band_to_osc
@@ -143,22 +159,28 @@ def eeg_stream_to_osc(stream_type, remove_aux):
         samples, timestamps = inlet.pull_chunk(
             timeout=LSL_PULL_TIMEOUT, max_samples=LSL_MAX_SAMPLES
         )
+
         if timestamps:
             last_received_time = time.time()
             stream_running = True
-            # Send raw signals
-            osc_client.send_message(f"/muse/{stream_type.lower()}", samples[0])
 
-            # Save buffer to compute features
-            samples = np.array(samples)[:, ::-1]
-            data = np.roll(data, -1, axis=0)
-            data[-1, :] = samples
-        else:
+            if use_aux:
+                incoming_data = samples[0]
+            else:
+                incoming_data = samples[0][:-1]
+
+            # Send raw signals
+            osc_client.send_message(f"/muse/{stream_type.lower()}", incoming_data)
+
+            # Save last datat to the fft buffer
+            fft_buffer = np.roll(fft_buffer, -1, axis=0)
+            fft_buffer[-1, :] = incoming_data
+        else:  # timeout mechanism to stop sending data when stream is lost
             if time.time() - last_received_time > 1 / CHECK_STREAM_RATE:
                 stream_running = False
 
 
-def lsl_to_osc(remove_aux):
+def lsl_to_osc(use_aux):
     found_stream = False
     print("Looking for LSL streams...")
     while not found_stream:
@@ -171,7 +193,7 @@ def lsl_to_osc(remove_aux):
     # Create processes that will stream data from LSL to OSC
     for type in streams_types:
         if type == "EEG":
-            process = mp.Process(target=eeg_stream_to_osc, args=(type, remove_aux))
+            process = mp.Process(target=eeg_stream_to_osc, args=(type, use_aux))
         else:
             process = mp.Process(target=single_lsl_stream_to_osc, args=(type,))
         process.start()
@@ -183,6 +205,8 @@ def lsl_to_osc(remove_aux):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--remove-aux", action="store_true", default=False)
+    parser.add_argument(
+        "--aux", action="store_true", default=False, help="Include Muse AUX channel"
+    )
     args, _ = parser.parse_known_args()
-    lsl_to_osc(args.remove_aux)
+    lsl_to_osc(use_aux=args.aux)